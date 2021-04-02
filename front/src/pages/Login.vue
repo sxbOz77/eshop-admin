8a5e50a75dee03b91325fb0536dc81585b024344
songxiongbin <ozgungnir77@foxmail.com>
2021-04-02 16:34:26 +0800

    login页面及其基本布局

---
+++ front/src/pages/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login-container">
+    <div class="login-box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+};
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login-box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,7 +3,16 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    path: "/login",
+    component: () => import("@/pages/Login"),
+  },
+];
 
 const router = new VueRouter({
   routes,
